da2edc029ecc970422d2da05aef1f39acb8577dd
Pâmela Borges <pamela_borges18@hotmail.com>
2022-09-07 22:09:38 -0300

    Delete de um desafio

--- Desafio 2764/IExportarDados.cs
+++
@@ -1,8 +0,0 @@
-namespace  Desafio2764
-{
-    //Interface para exportação de dados  tanto em console ,quanto em arquivo,ou qualquer outra forma de exporta��o
-    public interface IExportarDados
-    {
-        void Exportar(string[] datas);
-    }
-}
--- Desafio 2764/README.md
+++
@@ -1,7 +0,0 @@
-
-
-# Meus primeiros projetos
--Tecnologia usada -Dotnet Core 5
-
-# Sobre o que e esse código?
-- Resolução do exercício proposto ,entrada e saida de dados.
